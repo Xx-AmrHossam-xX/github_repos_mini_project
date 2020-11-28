60a3b417708465b60cfd638e9c095b04c302d90d
XxAmrHossamxX <amr.hossam@algorithmz.net>
2020-11-28 14:54:09 +0200

    Cleaned App component , communicated with endpoint and handled its errors

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import { useState, useEffect, Fragment } from 'react';
 import './App.css';
 import MainPage from './Pages/MainPage/MainPage';
 function App (){
-	const a = [ 1, 2, 3 ];
-	const b = [ 4, 5 ];
-	const [ abc, setabc ] = useState([]);
 	return (
 		<Fragment>
 			<MainPage />
-			<button className="btn btn-large btn-success" onClick={() => setabc(prev => [ ...prev, ...a ])}>
-				Click me
-			</button>
-			<button className="btn btn-large btn-danger" onClick={() => setabc(prev => [ ...prev, ...b ])}>
-				Click me
-			</button>
-			<ul>{abc ? abc.map((item, key) => <li key={key}>{item}</li>) : <li>Wait . . .</li>}</ul>
 		</Fragment>
 	);
 }
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -2,11 +2,33 @@ import { useState, useEffect, Fragment } from 'react';
 import './MainPage.css';
 import Repo from '../../Components/Repo/Repo';
 function MainPage (){
-	return (
-		<Fragment>
-			<h1>Main Page</h1>
-			<Repo />
-		</Fragment>
+	const [ TotalRepos, SetTotalRepos ] = useState([]);
+	const [ PageNo, SetPageNo ] = useState(1);
+	const [ Error, SetError ] = useState(false);
+	useEffect(
+		() =>
+			fetch(
+				`https://api.github.com/search/repositssssssories?q=created:>2017-10-22&sort=stars&order=desc&page=${PageNo}`
+			)
+				.then(response => response.json())
+				.then(data => {
+					console.log('data ', data);
+					if (data.message === 'Not Found') {
+						SetError(true);
+					} else {
+						SetError(false);
+						SetTotalRepos(prev => [ ...prev, ...data.items ]);
+					}
+				})
+				.catch(error => {
+					SetError(true);
+				}),
+		[]
 	);
+	if (Error) {
+		return <h1>An Unexpected Error has occured , please check your internet connectivity and refresh the page</h1>;
+	} else {
+		return <Fragment>{TotalRepos.length > 0 ? TotalRepos.map((item, key) => <Repo key={key} />) : null}</Fragment>;
+	}
 }
 export default MainPage;
