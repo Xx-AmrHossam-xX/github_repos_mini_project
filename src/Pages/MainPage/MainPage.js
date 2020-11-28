0e800c0412ae8b903d9f890c61fc88d8befdd95a
XxAmrHossamxX <amr.hossam@algorithmz.net>
2020-11-28 21:03:30 +0200

    Infinite Scrolling Feature

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -3,28 +3,39 @@ import './MainPage.css';
 import Repo from '../../Components/Repo/Repo';
 function MainPage (){
 	const [ TotalRepos, SetTotalRepos ] = useState([]);
-	const [ PageNo, SetPageNo ] = useState(1);
 	const [ Error, SetError ] = useState(false);
-	useEffect(
-		() =>
-			fetch(
-				`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${PageNo}`
-			)
-				.then(response => response.json())
-				.then(data => {
-					console.log('data ', data);
-					if (data.message === 'Not Found') {
-						SetError(true);
-					} else {
-						SetError(false);
-						SetTotalRepos(prev => [ ...prev, ...data.items ]);
-					}
-				})
-				.catch(error => {
+	let PageNo = 1;
+
+	useEffect(() => {
+		ApiRequest();
+		window.addEventListener('scroll', FetchMore);
+		return () => window.removeEventListener('scroll', FetchMore);
+	}, []);
+
+	function FetchMore (){
+		if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+			PageNo++;
+			ApiRequest();
+		}
+	}
+
+	const ApiRequest = () => {
+		console.log(PageNo, ' ??????????????????????????');
+		fetch(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${PageNo}`)
+			.then(response => response.json())
+			.then(data => {
+				console.log('data ', data);
+				if (data.message === 'Not Found') {
 					SetError(true);
-				}),
-		[]
-	);
+				} else {
+					SetError(false);
+					SetTotalRepos(prev => [ ...prev, ...data.items ]);
+				}
+			})
+			.catch(error => {
+				SetError(true);
+			});
+	};
 	if (Error) {
 		return <h1>An Unexpected Error has occured , please check your internet connectivity and refresh the page</h1>;
 	} else {
